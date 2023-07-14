88184d4f611c39b68d074920bb699a380e9ebf80
Govern0r <mike.tromba@gmail.com>
2023-07-14 11:54:08 -0400

    First commit

---
+++ src/index.ts
@@ -0,0 +1,103 @@
+import { and, or, eq, asc, desc, gt, lt, AnyColumn, SQL } from 'drizzle-orm'
+
+// With multiple cursors
+export function withCursorPagination<
+	PrimaryColumn extends AnyColumn,
+	// Make sure the secondary column is from the same table as the primary column:
+	TableColumns extends PrimaryColumn['table']['_']['columns'],
+	SecondaryColumn extends TableColumns[keyof TableColumns],
+	PrimaryCursor extends
+		| ReturnType<PrimaryColumn['mapFromDriverValue']>
+		| undefined,
+	SecondaryCursor extends
+		| ReturnType<SecondaryColumn['mapFromDriverValue']>
+		| undefined,
+	Order extends 'asc' | 'desc'
+>({
+	cursors,
+	limit,
+	where: inputWhere
+}: { limit: number; where?: Parameters<typeof and>[0] } & (
+	| {
+			cursors: [
+				[PrimaryColumn, Order] | [PrimaryColumn, Order, PrimaryCursor]
+			] // A single unique + sequential field
+	  }
+	| {
+			cursors: [
+				[PrimaryColumn, Order] | [PrimaryColumn, Order, PrimaryCursor], // A non-unique sequential field
+				(
+					| [SecondaryColumn, Order]
+					| [SecondaryColumn, Order, SecondaryCursor]
+				) // A unique field
+			]
+	  }
+)): {
+	orderBy: SQL<unknown>[]
+	limit: number
+	where?: SQL<unknown>
+} {
+	// Primary cursor
+	const primaryColumn = cursors[0][0]
+	const primaryOrder = cursors[0][1] === 'asc' ? asc : desc
+	const primaryOperator = cursors[0][1] === 'asc' ? gt : lt
+	const primaryCursor = cursors[0][2]
+
+	// Secondary cursor (unique fallback like an id field for a stable sort)
+	const secondaryColumn = cursors[1] ? cursors[1][0] : null
+	const secondaryOrder = cursors[1]
+		? cursors[1][1] === 'asc'
+			? asc
+			: desc
+		: null
+	const secondaryOperator = cursors[1]
+		? cursors[1][1] === 'asc'
+			? gt
+			: lt
+		: null
+	const secondaryCursor = cursors[1] ? cursors[1][2] : undefined
+
+	// Single cursor pagination
+	const singleColumnPaginationWhere =
+		typeof primaryCursor !== 'undefined'
+			? primaryOperator(primaryColumn, primaryCursor)
+			: null
+
+	// Double cursor pagination
+	const doubleColumnPaginationWhere =
+		secondaryColumn &&
+		secondaryOperator &&
+		typeof primaryCursor !== 'undefined' &&
+		typeof secondaryCursor !== 'undefined'
+			? or(
+					primaryOperator(primaryColumn, primaryCursor),
+					and(
+						eq(primaryColumn, primaryCursor),
+						secondaryOperator(secondaryColumn, secondaryCursor)
+					)
+			  )
+			: null
+
+	// Generate the final where clause
+	const paginationWhere = secondaryColumn
+		? doubleColumnPaginationWhere
+		: singleColumnPaginationWhere
+
+	const where = inputWhere
+		? paginationWhere
+			? and(inputWhere, paginationWhere)
+			: inputWhere
+		: paginationWhere
+
+	// Return object which can be easily spread into a query
+	return {
+		orderBy: [
+			primaryOrder(primaryColumn),
+			...(secondaryColumn && secondaryOrder
+				? [secondaryOrder(secondaryColumn)]
+				: [])
+		],
+		limit,
+		...(where ? { where } : {})
+	}
+}
